78d587957ffcee70338a3b300cfe93152afc1b87
hrishd <hrishd@abv.bg>
2024-01-08 21:23:36 +0200

    Adds dtos

---
+++ CompanyProjTracker/src/main/java/com/gglozanov/companyprojtracker/dto/EmployeeDTO.java
@@ -0,0 +1,8 @@
+public class EmployeeDTO {
+
+    private Long id;
+    private String name;
+    private Set<Long> projectIds;
+
+    // Getters and setters, constructors
+}
---
+++ CompanyProjTracker/src/main/java/com/gglozanov/companyprojtracker/dto/ProjectDTO.java
@@ -0,0 +1,8 @@
+public class ProjectDTO {
+
+    private Long id;
+    private String projectName;
+    private Long taskId;
+
+    // Getters and setters, constructors
+}
