bc205cc0468d269e0556126c83b38ef3976564cc
hrishd <hrishd@abv.bg>
2024-01-10 20:45:56 +0200

    fixed ProjectControllerTest

--- CompanyProjTracker/src/test/java/com/gglozanov/companyprojtracker/controller/ProjectControllerTest.java
+++ CompanyProjTracker/src/test/java/com/gglozanov/companyprojtracker/controller/ProjectControllerTest.java
@@ -1,22 +1,38 @@
 package com.gglozanov.companyprojtracker.controller;
 
+import com.gglozanov.companyprojtracker.DemoApplication;
 import com.gglozanov.companyprojtracker.model.Project;
 import com.gglozanov.companyprojtracker.repository.ProjectRepository;
 import com.gglozanov.companyprojtracker.controller.ProjectController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
-@WebMvcTest(ProjectController.class)
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class ProjectControllerTest {
 
     @Autowired
@@ -27,6 +43,11 @@ public class ProjectControllerTest {
 
     @InjectMocks
     private ProjectController projectController;
+
+    @BeforeEach
+    void setUp(WebApplicationContext wac) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new ProjectController(projectRepository)).build();
+    }
 
     @Test
     public void testGetAllProjects() throws Exception {
